be465bf8a278ef7d77f19bceee993fa06b64aca8
Wyatt C <wcastellan@yahoo.com>
2021-12-30 11:59:29 -0500

    fix and troubleshoot errors regarding the database

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
 // import express
 const express = require('express');
 const db = require('./db/connection');
-const fs = require('fs');
-const path = require('path');
-const res = require('express/lib/response');
+// add near top of file 
 const apiRoutes = require('./routes/apiRoutes');
+const inquirer = require('inquirer');
+
+// PORT designation
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 // express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(express.static('public'));
+
+// add after express middleware
 app.use('/api', apiRoutes);
 
-// default response for any other request (not found)
+// default response for amny other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
 });
@@ -22,6 +25,9 @@ app.use((req, res) => {
 db.connect(err => {
     if (err) throw err;
     console.log('Database connected.');
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
     afterConnection();
 });
 
